18de97508b84f9ca54cdbe12f08c9e08a49813ff
Jason Mottershead <jasonmottershead@hotmail.co.uk>
2020-11-12 17:06:44 +0000

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "yamp-cpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+		
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
